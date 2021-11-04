c1f1aaa2d38bec4131b6965b446e63b83d646aec
Miles Littlejohn <mjl6@email.sc.edu>
2021-11-04 00:47:59 -0400

    Adding UI navigation assets

--- Assets/TextMesh Pro/Scripts/Title Screen/MainMenu.cs
+++ Assets/TextMesh Pro/Scripts/Title Screen/MainMenu.cs
@@ -6,20 +6,20 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void NewGame() {
-        Debug.Log("this loads the new game scen");
-        /*SceneManager.LoadScene()*/
+        Debug.Log("this loads the new game scene");
+        /*SceneManager.LoadScene("NewCharacter")*/
     }
 
     public void LoadGame() {
-        /*SceneManager.LoadScene()*/
+        /*SceneManager.LoadScene("FileSelect")*/
     }
 
     public void Settings() {
-        /*SceneManager.LoadScene()*/
+        /*SceneManager.LoadScene("Settings")*/
     }
 
     public void Credits() {
-        /*SceneManager.LoadScene()*/
+        /*SceneManager.LoadScene("Credits")*/
     }
 
     public void Quit() {
