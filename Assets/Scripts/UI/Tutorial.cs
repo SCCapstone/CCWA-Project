0074021af1835923dc989afd566b5820cfab7033
Miles Littlejohn <mjl6@email.sc.edu>
2021-11-04 22:41:25 -0400

    Functionality on the tutorial Screen works

--- Assets/Scripts/UI/Tutorial.cs
+++ Assets/Scripts/UI/Tutorial.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //Will eventually be the texts we actually use in game
+    public string[] texts = {"This is the text for page 1.", "Undoubtably, the text for page 2.", "WOW! It's the text for page three."};
+    public int iterator = 0;
+    public int pageCounter = 1;
+    public TextMeshProUGUI infoText;
+    public Text pageCount;
+    
+    void Awake() {
+        infoText = GetComponentInChildren<TextMeshProUGUI>();
+        
+        //Counter must be at top of the scene
+        pageCount = GetComponentInChildren<Text>();
+    }
+
+    void Update() {
+        infoText.text = texts[iterator];
+        pageCount.text = pageCounter + "/3";
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void nextBtn() {
+        if (iterator < 2) {
+            ++iterator;
+            ++pageCounter;
+        }
+    }
+
+    public void backBtn() {
+        if (iterator > 0) {
+            --iterator;
+            --pageCounter;
+        } 
+    }
+    
+    public void mainBackBtn() {
+        SceneManager.LoadScene("Title Screen");
     }
 }
+
