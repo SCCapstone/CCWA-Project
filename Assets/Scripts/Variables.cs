6fd2c9a2262d1301c48ca466a44908f2c0d8e43b
ianm24 <ianmcdowell24@gmail.com>
2021-12-03 15:23:52 -0500

    can load variables from file, fixed filedata constructor, added gameplay to build

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -8,5 +8,13 @@ public class Variables:ScriptableObject
 
     //File Stuff
     public static bool newGame;
+
+    //Game state Stuff
+    public static bool inRun = false;
+    public static int playerHealth;
+    public static int playerStamina;
+    public static string playerClass;
+    public static int floorNum;
+    public static string floorSeed;
     
 }
