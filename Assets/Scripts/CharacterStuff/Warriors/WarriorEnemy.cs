ddfeb75e8695f5aa135eee42ff74aeec5d1490cc
Miles Littlejohn <mjl6@email.sc.edu>
2021-12-03 17:03:53 -0500

    testing enemy attack

--- Assets/Scripts/CharacterStuff/Warriors/WarriorEnemy.cs
+++ Assets/Scripts/CharacterStuff/Warriors/WarriorEnemy.cs
@@ -23,8 +23,32 @@ public class WarriorEnemy : Warrior
         Destroy(gameObject);
     }
 
-    public override void DamageHealth(int collision)
-    {
-        base.DamageHealth(collision);
+    public void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("we caressed under the wonder truck");
+        if (collision.tag == "Player") {
+            //Gets the instance of player
+            var playerCharacter = collision.GetComponent<RogueCharacter>();
+            
+            Debug.Log(playerCharacter);
+            if (playerCharacter == null) {
+                //playerCharacter = collision.GetComponent<RogueCharacter>();
+            }
+            
+            //calculating the damage done to player
+            int damage = attackDmg - playerCharacter.defense;
+
+            //Damages the player's health via the enemy's attackDmg value
+            if (damage <= 0) {
+                //Always do at least one damage to a player
+                playerCharacter.DamageHealth(1);
+            } else {
+                playerCharacter.DamageHealth(damage);
+            }
+            
+            //Kills the player if their health is less 0
+            if (playerCharacter.health <= 0) {
+                playerCharacter.Die();
+            }
+        }
     }
 }
