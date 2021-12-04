5715bb4442e147d1b52f2b7bc9da6b4619bf6570
ianm24 <ianmcdowell24@gmail.com>
2021-12-03 20:46:36 -0500

    pathfinding makes enemies go towards moving player

--- Assets/Scripts/Pathfinding/EnemyPathfinding.cs
+++ Assets/Scripts/Pathfinding/EnemyPathfinding.cs
@@ -7,10 +7,10 @@ public class EnemyPathfinding : MonoBehaviour
     private Transform target;
     private int WidthOfGrid = Constants.mapWidth;
     private int HeightOfGrid = Constants.mapHeight;
-    
+
 
     public int speed;
-    
+
     private Pathfinding enemyAi;
     private List<Vector3> pathVectorList;
     // Start is called before the first frame update
@@ -25,22 +25,29 @@ public class EnemyPathfinding : MonoBehaviour
 
     void Chasing()
     {
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
         pathVectorList = enemyAi.FindPath(GetPostion(), GetTargetPosition());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!(pathVectorList == null) && !(pathVectorList.Count == 0) )
+        // Debug.Log("target pos "+target.position.x + " " + target.position.y + " " + target.position.z);
+        if (!(pathVectorList == null) && !(pathVectorList.Count == 0))
         {
+
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, pathVectorList[0], step);
-            if(transform.position == pathVectorList[0])
+            // Debug.Log("moved towards "+pathVectorList[0].x + " " + pathVectorList[0].y + " " + pathVectorList[0].z);
+            if (transform.position == pathVectorList[0])
             {
+                // Debug.Log("Made it"+pathVectorList[0].x + " " + pathVectorList[0].y + " " + pathVectorList[0].z);
                 pathVectorList.RemoveAt(0);
             }
         }
-            
+
     }
 
 
