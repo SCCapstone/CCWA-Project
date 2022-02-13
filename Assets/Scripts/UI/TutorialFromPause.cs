c06672259abf07041d0da74da7d972202e255516
Miles Littlejohn <mjl6@email.sc.edu>
2022-02-13 02:51:32 -0500

    resized player character

---
+++ Assets/Scripts/UI/TutorialFromPause.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFromPause : MonoBehaviour
+{
+    public GameObject PauseOverlay;
+    public GameObject TutorialOverlay;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
