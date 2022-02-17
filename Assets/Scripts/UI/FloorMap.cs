b694ba1e4e76f93f96b61055a2d1edac399ce578
Ian McDowell <ianmcdowell24@gmail.com>
2022-02-16 19:35:59 -0500

    Finished minimap closes #113

--- Assets/Scripts/UI/FloorMap.cs
+++ Assets/Scripts/UI/FloorMap.cs
@@ -10,6 +10,7 @@ public class FloorMap : MonoBehaviour
     public Tilemap miniMap;
     public TileBase roomTile;
     public TileBase currRoomTile;
+    public TileBase noRoomTile;
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,9 @@ public class FloorMap : MonoBehaviour
         int yoffset = currFloor.floorLayout.GetLength(0);
         int xoffset = currFloor.floorLayout.GetLength(1);
         FloorGenerator.printFloor(currFloor.floorLayout);
+        int minimapTop = 17;
+        int minimapRight = 32;
+        // miniMap.SetTile(new Vector3Int(32,17,0), currRoomTile);
         for(int i=0; i<yoffset; i++) {
             for(int j=0; j<xoffset; j++) {
                 int room = currFloor.floorLayout[i,j];
@@ -31,11 +35,14 @@ public class FloorMap : MonoBehaviour
                 {
                     if(room == floorGenerator.currRoomIdx+1)
                     {
-                        miniMap.SetTile(new Vector3Int(j,yoffset-1-i,0), currRoomTile);
+                        miniMap.SetTile(new Vector3Int((minimapRight-(xoffset-1))+j,minimapTop-i,0), currRoomTile);
                     } else
                     {
-                        miniMap.SetTile(new Vector3Int(j,yoffset-1-i,0), roomTile);
+                        miniMap.SetTile(new Vector3Int((minimapRight-(xoffset-1))+j,minimapTop-i,0), roomTile);
                     }
+                } else
+                {
+                   miniMap.SetTile(new Vector3Int((minimapRight-(xoffset-1))+j,minimapTop-i,0), noRoomTile); 
                 }
             }
         }
