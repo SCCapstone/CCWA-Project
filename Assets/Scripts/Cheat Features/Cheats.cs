027ce1d14a671febd6457fd615db1419221b2d0e
Jonathan Terry <jonathanterry935@gmail.com>
2022-02-21 16:54:38 -0500

    Fixed cheats and attatched cheat script to camera

--- Assets/Scripts/Cheat Features/Cheats.cs
+++ Assets/Scripts/Cheat Features/Cheats.cs
@@ -5,26 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class Cheats : MonoBehaviour
 {
-
-    void DestroyAllEnemies() {
-        WarriorEnemy[] warriorEnemies = GameObject.FindObjectsOfType<WarriorEnemy>();
-        for(int i=0; i<warriorEnemies.Length; i++) {
-            Destroy(warriorEnemies[i]);
+    //End Key
+    void DisableEnemies() {
+        Debug.Log("Destroy Enemeies");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for(int i=1; i<enemies.Length; i++) {
+            Destroy(enemies[i]);
         }
+        
     }
 
+    //Page Up Key
     void MoveToNextRoom() {
+        Debug.Log("Next Room");
         var floorMap = GameObject.Find("Floor Map");
         FloorGenerator floorGenerator = floorMap.GetComponent<FloorGenerator>();
         RoomRenderer roomRenderer = floorMap.GetComponent<RoomRenderer>();
-        Room nextRoom = floorGenerator.GetCurrFloor().rooms[floorGenerator.currRoomIdx++];
-        RoomRenderer.SetCurrentRoom(nextRoom);
+        int nextIdx = floorGenerator.currRoomIdx == floorGenerator.GetCurrFloor().rooms.Length-1 ? floorGenerator.currRoomIdx : floorGenerator.currRoomIdx++;
+        Room nextRoom = floorGenerator.GetCurrFloor().rooms[nextIdx];
+        roomRenderer.setCurrentRoom(nextRoom);
+        roomRenderer.RenderRoom(nextRoom);
     }
 
+    //Home key
     void GenerateNewFloor() {
+        Debug.Log("New Floor");
+        DisableEnemies();
         var floorMap = GameObject.Find("Floor Map");
         FloorGenerator floorGenerator = floorMap.GetComponent<FloorGenerator>();
-        floorGenerator.NewFloor(Random.Range(1,10));
+        floorGenerator.NewFloor(Random.Range(1,5));
     }
 
     // Start is called before the first frame update
@@ -37,7 +46,7 @@ public class Cheats : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.End)) {
-            DestroyAllEnemies();
+            DisableEnemies();
         }
         else if(Input.GetKeyDown(KeyCode.PageUp)) {
             MoveToNextRoom();
