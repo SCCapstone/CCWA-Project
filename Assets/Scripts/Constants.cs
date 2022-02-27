a81eace4783fb71b31a20ce079df74719481ecb3
Miles Littlejohn <mjl6@email.sc.edu>
2022-02-27 16:51:29 -0500

    closes #106

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -15,5 +15,5 @@ public class Constants : ScriptableObject
     public const string SAVE_DIRECTORY = "./saves/";
     public const string SAVE_FILE_BASE_NAME = "savefile";
     public static readonly int[] VALID_FILE_NUMS = {1,2,3};
-    public static readonly string[] ALL_ACHIEVEMENTS = {""}; //TODO add all achievement titles
+    public static readonly string[] ALL_ACHIEVEMENTS = {""}; //TODO add all achievement title
     }
