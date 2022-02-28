4ada547f6202fb4ff4c9c7f4fac0c49622566f8d
Jonathan Terry <jonathanterry935@gmail.com>
2022-02-27 22:50:06 -0500

    Adds a camera tracker to follow the player around the map

---
+++ Assets/Scripts/CharacterStuff/CameraTracker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public Transform trackedObject;
+    public float updateSpeed = 3;
+    public Vector2 trackingOffset;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+
+    void Awake() {
+        trackedObject = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Start() {
+        offset = (Vector3)trackingOffset;
+        offset.z = transform.position.z - trackedObject.position.z;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = Vector3.MoveTowards(transform.position,
+            trackedObject.position + offset, updateSpeed * Time.deltaTime);
+    }
+}
