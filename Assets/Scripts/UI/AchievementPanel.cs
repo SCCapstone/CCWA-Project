f9a6f966bffe07d2322d4531ac1647a069369a23
ianm24 <ianmcdowell24@gmail.com>
2022-03-29 21:42:18 -0400

    added remaining achievements; progress on #176

--- Assets/Scripts/UI/AchievementPanel.cs
+++ Assets/Scripts/UI/AchievementPanel.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class AchievementPanel : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject panel2;
     public GameObject title;
+    public GameObject title2;
     public GameObject description;
+    public GameObject description2;
     public bool ach01Trigger = false;
     public bool ach02Trigger = false;
+    public bool ach03Trigger = false;
+    public bool ach04Trigger = false;
+    public bool ach05Trigger = false;
     // Update is called once per frame
 
     void Update()
@@ -26,6 +32,9 @@ public class AchievementPanel : MonoBehaviour
 
         ach01Trigger = Variables.achievementTriggers[0];
         ach02Trigger = Variables.achievementTriggers[1];
+        ach03Trigger = Variables.achievementTriggers[2];
+        ach04Trigger = Variables.achievementTriggers[3];
+        ach05Trigger = Variables.achievementTriggers[4];
 
         if(ach01Trigger)
         {
@@ -34,6 +43,18 @@ public class AchievementPanel : MonoBehaviour
         if(ach02Trigger)
         {
             StartCoroutine(Achieve02());
+        }
+        if(ach03Trigger)
+        {
+            StartCoroutine(Achieve03());
+        }
+        if(ach04Trigger)
+        {
+            StartCoroutine(Achieve04());
+        }
+        if(ach04Trigger)
+        {
+            StartCoroutine(Achieve05());
         }
     }
 
@@ -56,6 +77,39 @@ public class AchievementPanel : MonoBehaviour
         yield return new WaitForSeconds(3);
         panel.SetActive(false);
         Variables.achievementTriggers[1] = false;
-        ach01Trigger = false;
+        ach02Trigger = false;
+    }
+    
+    IEnumerator Achieve03()
+    {
+        title.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_TITLES[2];
+        description.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_DESCRIPTIONS[2];
+        panel.SetActive(true);
+        yield return new WaitForSeconds(3);
+        panel.SetActive(false);
+        Variables.achievementTriggers[2] = false;
+        ach03Trigger = false;
+    }
+
+    IEnumerator Achieve04()
+    {
+        title.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_TITLES[3];
+        description.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_DESCRIPTIONS[3];
+        panel.SetActive(true);
+        yield return new WaitForSeconds(3);
+        panel.SetActive(false);
+        Variables.achievementTriggers[3] = false;
+        ach04Trigger = false;
+    }
+
+    IEnumerator Achieve05()
+    {
+        title2.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_TITLES[4];
+        description2.GetComponent<TMPro.TextMeshProUGUI>().text = Constants.ALL_ACHIEVEMENT_DESCRIPTIONS[4];
+        panel2.SetActive(true);
+        yield return new WaitForSeconds(3);
+        panel2.SetActive(false);
+        Variables.achievementTriggers[4] = false;
+        ach05Trigger = false;
     }
 }
