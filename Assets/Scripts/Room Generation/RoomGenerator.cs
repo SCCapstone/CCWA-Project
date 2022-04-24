cbc73e43869578df48277600cfcf7aa55e3de44b
Jonathan Terry <jonathanterry935@gmail.com>
2022-04-24 15:54:10 -0400

    Tests for RoomGenerator Methods

--- Assets/Scripts/Room Generation/RoomGenerator.cs
+++ Assets/Scripts/Room Generation/RoomGenerator.cs
@@ -33,6 +33,7 @@ public class RoomGenerator {
     // Begin Constructors**********************************************************************************************
 
     //Construct a RoomGenerator capabale of generating rooms of HeightxWidth size
+    // Has a unit test
     public RoomGenerator(bool useSeed) {
         this.useSeed = useSeed;
     }
@@ -40,6 +41,7 @@ public class RoomGenerator {
 
     // Begin room generation functions*********************************************************************************
 
+    // Has a unit test
     public int[,] CreateRoomBorder(int[,] map) {
         for(int i=0; i<height; i++) {
             for(int j=0; j<width; j++) {
@@ -52,6 +54,7 @@ public class RoomGenerator {
         return map;
     }
 
+    // Has a unit test
     public int[,] ClearSpawns(int[,] map) {
         for(int i=15; i<24; i++) {
             for(int j=10; j<15; j++) {
@@ -72,6 +75,7 @@ public class RoomGenerator {
         return map;
     }
 
+    // Has a unit test
     public int[,] GenerateRoomWithPerlinNoise (string seed) {
         System.Random rand = new System.Random(seed.GetHashCode());
         int n = rand.Next();
@@ -92,10 +96,12 @@ public class RoomGenerator {
         return map;
     }
 
+    // Has a unit test
     public int[,] GenerateRoomMap(string seed) {
         return ClearSpawns(CreateRoomBorder(GenerateRoomWithPerlinNoise(seed)));
     }
 
+    //Has a unit test
     public Room GenerateRoom(string seed, int numEnemies, int numItems, bool bossRoom, string[] directions) {
         // Generate a randomly filled Room map
         // Iterate over map to create a Room
@@ -122,13 +128,14 @@ public class RoomGenerator {
     // End Room Generation Functions***********************************************************************************
 
     // Begin Spawn Location Generation Functions***********************************************************************
-    void GenerateSpawnLocations(Room room) {
+    public void GenerateSpawnLocations(Room room) {
         GenerateEnemySpawns(room.numEnemies);
         GenerateLootSpawns(room.numItems);
         //GenerateExitSpawns(map, tilemap, exitTile);
     }
 
-    Location[] GenerateEnemySpawns(int numEnemies) {
+    // Has a unit test
+    public Location[] GenerateEnemySpawns(int numEnemies) {
         Location[] newEnemyLocations = new Location[numEnemies];
         int tempEnemyCounter = 0;
         for(int i = height-3; i>height-7; i--) {
@@ -152,7 +159,8 @@ public class RoomGenerator {
         return newEnemyLocations;
     }
 
-    Location[] GenerateLootSpawns(int numItems) {
+    // Has a unit test
+    public Location[] GenerateLootSpawns(int numItems) {
         Location[] newItemLocations = new Location[numItems];
         int tempItemCounter = 0;
         for(int i = height-3; i>height-7; i--) {
@@ -177,7 +185,8 @@ public class RoomGenerator {
 
     // This method generates a single exit location in a room
     // It takes a direction (north, south, etc.) as a parameter, and returns a Location
-    Location GenerateExitSpawn(string direction, int[,] map) {
+    // Has a unit test
+    public Location GenerateExitSpawn(string direction, int[,] map) {
         switch(direction) {
             case "east":
                     for(int i = height- 3; i>height-7; i--) {
@@ -219,7 +228,8 @@ public class RoomGenerator {
         return new Location();
     }
 
-    Location[] GenerateMultipleExits(int numOfLocations, string[] directions, int[,] map) {
+    // Has a unit test
+    public Location[] GenerateMultipleExits(int numOfLocations, string[] directions, int[,] map) {
         Location[] newLocations = new Location[numOfLocations];
         for(int i=0; i<directions.Length; i++) {
             newLocations[i] = GenerateExitSpawn(directions[i], map);
--- Assets/Tests/Unit_Tests/RoomGeneratorTests.cs
+++ Assets/Tests/Unit_Tests/RoomGeneratorTests.cs
@@ -6,12 +6,15 @@ using UnityEngine.TestTools;
 
 public class RoomGeneratorTests
 {
+
+    RoomGenerator rg = new RoomGenerator(true);
+
     // A Test behaves as an ordinary method
     [Test]
     public void GenerateRoomTest()
     {
         // Use the Assert class to test conditions
-        RoomGenerator rg = new RoomGenerator(true);
+        // RoomGenerator rg = new RoomGenerator(true);
         Room room  = rg.GenerateRoom("test",1,1,false,new string[1]{"north"});
 
         Assert.AreEqual("test",room.seed);
@@ -21,5 +24,83 @@ public class RoomGeneratorTests
         Assert.AreEqual(1,room.itemLocations.Length);
         Assert.AreEqual(1,room.exitLocations.Length);
         
+    }
+
+    [Test]
+    public void GenerateRoomMapTest() {
+        int[,] testMap = rg.GenerateRoomMap("test");
+        Assert.IsNotNull(testMap);
+        Assert.AreEqual(testMap.Length, 625); //25*25=625
+    }
+
+    [Test]
+    public void GenerateRoomWithPerlinNoiseTest() {
+        int[,] testMap = rg.GenerateRoomWithPerlinNoise("test");
+        Assert.IsNotNull(testMap);
+        Assert.AreEqual(testMap.Length, 625); // 25*25=625
+    }
+
+    [Test]
+    public void ClearSpawnsTest() {
+        int[,] testMap = rg.GenerateRoomWithPerlinNoise("test");
+        int[,] clearedSpawnsMap = rg.ClearSpawns(testMap);
+        Assert.IsNotNull(testMap);
+        Assert.IsNotNull(clearedSpawnsMap);
+        Assert.AreNotEqual(testMap, clearedSpawnsMap);
+    }
+
+    [Test]
+    public void CreateRoomBorderTest() {
+        int[,] testMap = rg.GenerateRoomWithPerlinNoise("test");
+        int [,] borderedMap = rg.CreateRoomBorder(testMap);
+        Assert.IsNotNull(testMap);
+        Assert.IsNotNull(borderedMap);
+        Assert.AreNotEqual(testMap, borderedMap);
+    }
+
+    [Test]
+    public void RoomGeneratorConstructorTest() {
+        RoomGenerator roomFromConstructor = new RoomGenerator(true);
+        Assert.IsNotNull(roomFromConstructor);
+        Assert.AreEqual(roomFromConstructor.useSeed, true);
+    }
+
+    [Test]
+    public void GenerateEnemySpawnsTest() {
+        int numEnemies = 5;
+        Location[] testEnemyLocations = rg.GenerateEnemySpawns(numEnemies);
+        Assert.IsNotNull(testEnemyLocations);
+        Assert.AreEqual(testEnemyLocations.Length, numEnemies);
+    }
+
+    [Test]
+    public void GenerateLootSpawnsTest() {
+        int numItems = 3;
+        Location[] testLootLocations = rg.GenerateEnemySpawns(numItems);
+        Assert.IsNotNull(testLootLocations);
+        Assert.AreEqual(testLootLocations.Length, numItems);
+    }
+
+    [Test]
+    public void GenerateSingleExitSpawnTest() {
+        string testDirection = "north";
+        int[,] testMap = rg.GenerateRoomWithPerlinNoise("test");
+        Location testLocation = rg.GenerateExitSpawn(testDirection, testMap);
+        Assert.IsNotNull(testMap);
+        Assert.IsNotNull(testLocation);
+        Assert.AreEqual(testLocation.location, testDirection);
+    }
+
+    [Test]
+    public void GenerateMultipleExitsTest() {
+        int[,] testMap = rg.GenerateRoomWithPerlinNoise("test");
+        string[] testDirections = new string[]{"north", "east"};
+        int numOfLocations = 2;
+        Location[] multipleExits = 
+            rg.GenerateMultipleExits(numOfLocations, testDirections, testMap);
+        Assert.IsNotNull(testMap);
+        Assert.IsNotNull(testDirections);
+        Assert.AreEqual(numOfLocations, testDirections.Length);
+        Assert.AreEqual(multipleExits.Length, numOfLocations);
     }
 }
