bb22417d00ac57d8b4f0b0ff906e7eb8a309a9b4
Jonathan Terry <jonathanterry935@gmail.com>
2022-04-25 15:26:30 -0400

    Adds keys and the rest of the pickups to the map

--- Assets/Scripts/Room Generation/RoomRenderer.cs
+++ Assets/Scripts/Room Generation/RoomRenderer.cs
@@ -17,7 +17,9 @@ public class RoomRenderer : MonoBehaviour
     public TileBase exitTile;
     public TileBase lockedExitTile;
     public GameObject attackUp;
+    public GameObject defenseUp;
     public GameObject health;
+    public GameObject key;
     public GameObject enemy;
     public GameObject boss;
     public Room currentRoom;
@@ -60,7 +62,7 @@ public class RoomRenderer : MonoBehaviour
         // Iterate again; place exit locations
         for(int i=0; i<room.exitLocations.Length; i++) {
             System.Random rng = new System.Random();
-            int isLockedExit = rng.Next(1,20);
+            int isLockedExit = rng.Next(1,2);
             Location l = room.exitLocations[i];
             if (isLockedExit == 1){
                 floorMap.SetTile(new Vector3Int(l.locX, l.locY,0), lockedExitTile);
@@ -75,11 +77,15 @@ public class RoomRenderer : MonoBehaviour
         for(int i=0; i<room.itemLocations.Length; i++) {
             Location l = room.itemLocations[i];
             if(room.map[l.locX, l.locY] != 1) {
-                if(i%2 == 0) {  //health
+                if(i%4 == 0) {  //health
                     Instantiate(health, new Vector3(l.locX, l.locY, -1), Quaternion.identity);
                 }
-                else {  //attack up
+                else if (i%3 ==0) {  //attack up
                     Instantiate(attackUp, new Vector3(l.locX, l.locY, -1), Quaternion.identity);
+                } else if (i%2 == 0) {
+                    Instantiate(key, new Vector3(l.locX, l.locY, -1), Quaternion.identity);
+                } else {
+                    Instantiate(defenseUp, new Vector3(l.locX, l.locY, -1), Quaternion.identity);
                 }
             }
         }
@@ -118,33 +124,37 @@ public class RoomRenderer : MonoBehaviour
                 int pX = Math.Abs(x);
                 int pY = Math.Abs(y);
 
-                if(pX == l.locX && pY == l.locY) {
-                    string direction = l.location;
-                    for(int j=0; j<currFloor.rooms.Length; j++) {
-                        Room newRoom = currFloor.rooms[j];
-                        if(direction == "north" && Array.Exists(newRoom.exitLocations, l => l.location == "south")) {
-                            setCurrentRoom(newRoom);
-                            Variables.currentRoom = newRoom;
-                            RenderRoom(newRoom);
-                            floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
-                            player.transform.position = new Vector3(10,10,-0.32f);
-                        } else if(direction == "south" && Array.Exists(newRoom.exitLocations, l => l.location =="north")) {
-                            setCurrentRoom(newRoom);
-                            Variables.currentRoom = newRoom;
-                            RenderRoom(newRoom);
-                            floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
-                            player.transform.position = new Vector3(10,10,-0.32f);
-                        } else if(direction == "west" && Array.Exists(newRoom.exitLocations, l => l.location =="east")) {
-                            setCurrentRoom(newRoom);
-                            Variables.currentRoom = newRoom;
-                            RenderRoom(newRoom);
-                            floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
-                            player.transform.position = new Vector3(10,10,-0.32f);
-                            setCurrentRoom(newRoom);
-                            Variables.currentRoom = newRoom;
-                            RenderRoom(newRoom);
-                            floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
-                            player.transform.position = new Vector3(10,10,-0.32f);
+                TileBase doorTile = floorMap.GetTile(new Vector3Int(l.locX, l.locY, 0));
+                
+                if(doorTile != lockedExitTile || player.GetComponent<Character>().keyAmt >= 1) {
+                    if(pX == l.locX && pY == l.locY) {
+                        string direction = l.location;
+                        for(int j=0; j<currFloor.rooms.Length; j++) {
+                            Room newRoom = currFloor.rooms[j];
+                            if(direction == "north" && Array.Exists(newRoom.exitLocations, l => l.location == "south")) {
+                                setCurrentRoom(newRoom);
+                                Variables.currentRoom = newRoom;
+                                RenderRoom(newRoom);
+                                floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
+                                player.transform.position = new Vector3(10,10,-0.32f);
+                            } else if(direction == "south" && Array.Exists(newRoom.exitLocations, l => l.location =="north")) {
+                                setCurrentRoom(newRoom);
+                                Variables.currentRoom = newRoom;
+                                RenderRoom(newRoom);
+                                floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
+                                player.transform.position = new Vector3(10,10,-0.32f);
+                            } else if(direction == "west" && Array.Exists(newRoom.exitLocations, l => l.location =="east")) {
+                                setCurrentRoom(newRoom);
+                                Variables.currentRoom = newRoom;
+                                RenderRoom(newRoom);
+                                floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
+                                player.transform.position = new Vector3(10,10,-0.32f);
+                                setCurrentRoom(newRoom);
+                                Variables.currentRoom = newRoom;
+                                RenderRoom(newRoom);
+                                floorMap.GetComponent<FloorGenerator>().currRoomIdx = Array.IndexOf(currFloor.rooms,newRoom);
+                                player.transform.position = new Vector3(10,10,-0.32f);
+                            }
                         }
                     }
                 }
