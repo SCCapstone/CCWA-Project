cbd67d0749363eef20330d9ac879af0a8902f22f
Ian McDowell <ianmcdowell24@gmail.com>
2022-04-25 15:33:50 -0400

    added the rest of the Floor Generator tests

--- Assets/Tests/Behavioral_Tests/FloorGeneratorTests.cs
+++ Assets/Tests/Behavioral_Tests/FloorGeneratorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -6,19 +7,96 @@ using UnityEngine.TestTools;
 
 public class FloorGeneratorTests {
 
-    // A Test behaves as an ordinary method
-    [UnityTest]
-    public IEnumerator GenerateFloorTest()
+    public FloorGenerator GetTestFloorGenerator(string seed)
     {
-        // Use the Assert class to test conditions
         GameObject fm = GameObject.Instantiate(new GameObject("FileManager"));
         fm.AddComponent<FileManager>();
-        Variables.floorSeed = "test";
+        Variables.floorSeed = seed;
         Variables.newGame = true;
         FloorGenerator fg = fm.AddComponent<FloorGenerator>();
-        yield return new WaitForSeconds(0.2f);
-        Floor f1 = fg.GetCurrFloor();
-        
+        return fg;
+    }
+
+    // Floor Generation Tests
+    [UnityTest]
+    public IEnumerator GenerateFloorSeedTest()
+    {
+        Floor f1 = GetTestFloorGenerator("test").GetCurrFloor();   
         Assert.AreEqual("1test",f1.seed);
+        yield return new WaitForSeconds(0f);
+    }
+
+    [UnityTest]
+    public IEnumerator GenerateFloorShortSeedTest()
+    {
+        Floor f1 = GetTestFloorGenerator("f").GetCurrFloor();   
+        Assert.AreEqual("1f1f1f",f1.seed);
+        yield return new WaitForSeconds(0f);
+    }
+
+    [UnityTest]
+    public IEnumerator GenerateFloorNumRoomsTest()
+    {
+        Floor f1 = GetTestFloorGenerator("test").GetCurrFloor();
+        Assert.AreEqual(10,f1.rooms.Length);
+        yield return new WaitForSeconds(0f);
+    }
+
+    [UnityTest]
+    public IEnumerator GenerateFloorLayoutSizeTest()
+    {
+        Floor f1 = GetTestFloorGenerator("test").GetCurrFloor();
+        Assert.That(f1.floorLayout.Length, Is.GreaterThan(9));
+        yield return new WaitForSeconds(0f);
+    }
+
+    [UnityTest]
+    public IEnumerator GenerateFloorLayoutValuesTest()
+    {
+        int[,] layout = GetTestFloorGenerator("test").GetCurrFloor().floorLayout;
+        bool correctLayout = true;
+        for(int i = 0; i<layout.GetLength(0);i++)
+        {
+            for(int j = 0; j<layout.GetLength(1);j++)
+            {
+                if(layout[i,j]<0 || layout[i,j]>10)
+                {
+                    correctLayout = false;
+                    break;
+                }
+            }
+        }
+        Assert.That(correctLayout);
+        yield return new WaitForSeconds(0f);
+    }
+
+    // GetDirectionsFromExitString Tests
+    [UnityTest]
+    public IEnumerator GetDirFromExitStringOutputTest()
+    {
+        FloorGenerator fg = GetTestFloorGenerator("test");
+        string[] test = {"north","east","west","south"};
+        Assert.AreEqual(fg.GetDirectionsFromExitString("news"), test);
+        yield return new WaitForSeconds(0f);
+    }
+
+    [UnityTest]
+    public IEnumerator GetDirFromExitStringInvalidInputTest()
+    {
+        FloorGenerator fg = GetTestFloorGenerator("test");
+        string[] test = {"north","east","west","south",null,null,null,"east",null};
+        Assert.AreEqual(fg.GetDirectionsFromExitString("newspaper"), test);
+        yield return new WaitForSeconds(0f);
+    }
+
+    // GetRoomExits Tests
+    [UnityTest]
+    public IEnumerator GetRoomExitsOutputTest()
+    {
+        FloorGenerator fg = GetTestFloorGenerator("test");
+        int[,] layout = {{1,2},{3,0}};
+        string[,] test = {{"es","w"},{"n","0"}};
+        Assert.AreEqual(fg.GetRoomExits(layout), test);
+        yield return new WaitForSeconds(0f);
     }
 }
