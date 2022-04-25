11efae0d80a669c8ff0f0e8fe0c819d83f849c4a
Jonathan Terry <jonathanterry935@gmail.com>
2022-04-25 17:24:58 -0400

    Fix a problem with stuff

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     public Text scoreUI;
-    public GameObject scoreText;
+    // public GameObject scoreText;
     // Start is called before the first frame update
     void Awake()
     {
-        scoreUI.GetComponent<UnityEngine.UI.Text>().text ="Score: "+Variables.score;
+        scoreUI.text ="Score: "+Variables.score;
     }
 
   
     // Update is called once per frame
     void Update()
     {
-        scoreUI.GetComponent<UnityEngine.UI.Text>().text ="Score: "+Variables.score;
+        scoreUI.text ="Score: "+Variables.score;
     }
 }
